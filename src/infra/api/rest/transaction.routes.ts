30681a4f80f722e0c572815728c1a13ac90d2d35
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-14 23:12:51 -0300

    add transaction routes

--- src/infra/api/rest/transaction.routes.ts
+++ src/infra/api/rest/transaction.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { Transaction } from '../../../domain/entities/transaction';
-import { validationMiddleware } from '../validator/middlewares/validatorMiddleware';
+import { validateMiddleware } from '../validator/middlewares/validatorMiddleware';
 import { TransactionController } from "./transaction-controller";
 
 const transactionController = new TransactionController()
 const transactionRoutes = Router();
 
-transactionRoutes.post("/transaction", validationMiddleware(Transaction), transactionController.create);
+transactionRoutes.post("/transaction", validateMiddleware(Transaction), transactionController.create);
 
 export { transactionRoutes };
