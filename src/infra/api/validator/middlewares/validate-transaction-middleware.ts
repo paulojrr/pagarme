ed3130b27d9d31b216457dc90ba691d7d97cd201
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-19 18:41:08 -0300

    refactor middleware validate

---
+++ src/infra/api/validator/middlewares/validate-transaction-middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+
+const schema = Joi.object({
+  value: Joi.number().positive().required(),
+  description: Joi.string().required(),
+  paymentMethod: Joi.string().valid("credit_card", "debit_card").required(),
+  cardNumber: Joi.string().creditCard().required(),
+  cardHolderName: Joi.string().required(),
+  validFrom: Joi.date().iso().required(),
+  verificationNumber: Joi.string().length(3).required(),
+});
+
+export function validateTransaction(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { error } = schema.validate(request.body);
+  if (error) {
+    return response.status(400).json({ message: error.details[0].message });
+  }
+  return next();
+}
