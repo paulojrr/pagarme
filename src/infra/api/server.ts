03602e6be6e0f619ccb2899aaaaed06322d38415
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-19 19:53:46 -0300

    add validacao de env de test

--- src/infra/api/server.ts
+++ src/infra/api/server.ts
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(transactionRoutes);
 
-app.listen(3000, () => console.log("Server is running at port 3000"));
+if (process.env.NODE_ENV !== "test") {
+  app.listen(3000, () => console.log("Server is running at port 3000"));
+}
 
 export { app };
