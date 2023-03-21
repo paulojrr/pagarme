15713639a2ab10f1a112c2dff31c29016416f14d
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-21 19:49:08 -0300

    refactor create transaction

--- src/domain/usecases/create-transaction-usecase.ts
+++ src/domain/usecases/create-transaction-usecase.ts
@@ -1,4 +1,5 @@
 import { TransactionPostgresRepository } from "../../infra/repositories/prisma/transaction-repository";
+import { calculateFee, calculatePaymentDate, checkPaymentStatus } from "../helper/transaction-helper";
 import { CreateTransaction, ResponseTransaction } from "./transaction-dto";
 
 export class CreateTransactionUseCase {
@@ -7,18 +8,17 @@ export class CreateTransactionUseCase {
   ) { }
 
   async create(data: CreateTransaction): Promise<ResponseTransaction[]> {
-    data.cardNumber = data.cardNumber.slice(-4);
+    let { cardNumber, paymentMethod, value } = data
+    cardNumber = cardNumber.slice(-4);
 
-    const status = data.paymentMethod === 'debit_card' ? 'paid' : 'waiting_funds'
+    const status = checkPaymentStatus(paymentMethod)
+    const fee = calculateFee(paymentMethod, value)
+    const paymentDate = calculatePaymentDate(paymentMethod)
 
-    const date = new Date()
-    const value = data.paymentMethod === 'debit_card' ? data.value * 0.3 : data.value * 0.5
-
-    const paymentDate = data.paymentMethod === 'debit_card' ? date : new Date(date.setDate(date.getDate() + 30))
     const payables = {
       ...data,
       payables: {
-        value,
+        value: fee,
         status,
         paymentDate
       }
