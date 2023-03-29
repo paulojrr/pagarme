adc60373fe304a895148a760c06120c1e8483590
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:20:41 -0300

    add interface findTransactionsAvailable e findTransactionsWaitingFunds

--- src/domain/interfaces/repositories/transaction-repository.ts
+++ src/domain/interfaces/repositories/transaction-repository.ts
@@ -1,6 +1,8 @@
-import { CreateTransaction, ResponseTransaction } from "../../usecases/transaction-dto";
+import { CreateTransaction, ResponseTransaction, ResponseTransactionAvailable } from "../../usecases/transaction-dto";
 
 export interface TransactionRepository {
   create(data: CreateTransaction): Promise<ResponseTransaction[]>;
   findAll(): Promise<ResponseTransaction[]>;
+  findTransactionsAvailable(cpf: string): Promise<ResponseTransactionAvailable[]>;
+  findTransactionsWaitingFunds(cpf: string): Promise<ResponseTransaction[]>;
 }
