0f67f05686d26ccfdfee850100a53746438b9ab9
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:18:30 -0300

    refactor in memory repository

--- src/infra/repositories/in-memory/transaction-repository-in-memory.ts
+++ src/infra/repositories/in-memory/transaction-repository-in-memory.ts
@@ -1,16 +1,36 @@
-import { Transaction } from "@prisma/client";
 import { TransactionRepository } from "../../../domain/interfaces/repositories/transaction-repository";
+import { CreateTransaction, ResponseTransaction } from "../../../domain/usecases/transaction-dto";
 
 export class TransactionPostgresRepositoryInMemory
   implements TransactionRepository {
-  private transactions: Transaction[] = [];
+  private transactions: CreateTransaction[] = [];
+  private responseTransactions: ResponseTransaction[] = [];
 
-  async create(data: Transaction): Promise<Transaction[]> {
-    this.transactions.push(data);
-    return [data];
+  async create(data: CreateTransaction): Promise<ResponseTransaction[]> {
+    this.transactions.push();
+
+    let responseData = {
+      id: '41c21524-0708-493f-9f2e-a3a0938f8338',
+      value: data.value,
+      cpf: data.cpf,
+      description: data.description,
+      paymentMethod: data.paymentMethod,
+      cardNumber: data.cardNumber.slice(-4),
+      cardHolderName: data.cardHolderName,
+      validFrom: data.validFrom,
+      verificationNumber: data.verificationNumber,
+      payablesId: '41c21524-0708-493f-9f2e-a3a0938f8338'
+    }
+
+    this.responseTransactions.push(responseData)
+    return this.responseTransactions
   }
 
-  async findAll(): Promise<Transaction[]> {
-    return this.transactions;
+  async findAll(): Promise<ResponseTransaction[]> {
+    return this.responseTransactions
+  }
+
+  async findTransactions(cpf: string): Promise<ResponseTransaction[]> {
+    return this.responseTransactions.filter((transactions => transactions.cpf === cpf))
   }
 }
