1365da28cfcbd7f1c8ebf619ba7a79657843cc3f
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:13:55 -0300

    add funcao getAvailable

--- src/infra/api/rest/transaction-controller.ts
+++ src/infra/api/rest/transaction-controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import { CreateTransactionUseCase } from "../../../domain/usecases/create-transaction-usecase";
+import { GetAvailableTransactionUseCase } from "../../../domain/usecases/get-available-transaction-usecase";
 import { TransactionPostgresRepository } from "../../repositories/prisma/transaction-repository";
+
+interface RequestQuery {
+  cpf: string
+}
 
 export class TransactionController {
   constructor(
     private readonly createTransaction: CreateTransactionUseCase,
+    private readonly getAvailableTransaction: GetAvailableTransactionUseCase,
     private readonly transactionRepository: TransactionPostgresRepository
   ) { }
 
@@ -12,6 +18,30 @@ export class TransactionController {
     try {
       await this.createTransaction.create(request.body);
       const transaction = await this.transactionRepository.findAll();
+
+      return response.json(transaction);
+    } catch (error: any) {
+      console.log(error);
+      return response.status(400).json({ message: error.message });
+    }
+  }
+
+  async getAvailable(request: Request, response: Response) {
+    try {
+      const { cpf } = request.query as unknown as RequestQuery
+      const transaction = await this.getAvailableTransaction.getAvailable(cpf);
+
+      return response.json(transaction);
+    } catch (error: any) {
+      console.log(error);
+      return response.status(400).json({ message: error.message });
+    }
+  }
+
+  async getWaitingFunds(request: Request, response: Response) {
+    try {
+      const { cpf } = request.query as unknown as RequestQuery
+      const transaction = await this.transactionRepository.findTransactionsWaitingFunds(cpf);
 
       return response.json(transaction);
     } catch (error: any) {
