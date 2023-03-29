3f17bc4a56880587160a665b89c310082fce208e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:19:09 -0300

    corrige tests

--- src/domain/usecases/create-transaction.spec.ts
+++ src/domain/usecases/create-transaction.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { TransactionRepository } from "../../domain/interfaces/repositories/transaction-repository";
 import { TransactionPostgresRepositoryInMemory } from "../../infra/repositories/in-memory/transaction-repository-in-memory";
+import { calculatePaymentDate } from "../helper/transaction-helper";
 import { CreateTransactionUseCase } from "./create-transaction-usecase";
 
 describe("create transaction", () => {
@@ -16,22 +17,31 @@ describe("create transaction", () => {
 
   const payloadMock = {
     value: 100.0,
+    cpf: "079.430.010-36",
     description: "Test",
     paymentMethod: "credit_card",
     cardNumber: "4006604842674066",
     cardHolderName: "John Doe",
     validFrom,
     verificationNumber: "123",
+    payables: {
+      value: 100,
+      status: "waiting_funds",
+      paymentDate: calculatePaymentDate('credit_card')
+    }
   };
 
   const payloadReturn = {
+    id: '41c21524-0708-493f-9f2e-a3a0938f8338',
     value: 100.0,
+    cpf: "079.430.010-36",
     description: "Test",
     paymentMethod: "credit_card",
     cardNumber: "4066",
     cardHolderName: "John Doe",
     validFrom,
     verificationNumber: "123",
+    payablesId: '41c21524-0708-493f-9f2e-a3a0938f8338'
   };
 
   it("should create and return a transaction", async () => {
