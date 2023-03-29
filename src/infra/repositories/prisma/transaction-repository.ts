31c22d235e63c631646139213ddd88076220f680
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:16:45 -0300

    add funcoes find available e find waiting funds

--- src/infra/repositories/prisma/transaction-repository.ts
+++ src/infra/repositories/prisma/transaction-repository.ts
@@ -1,5 +1,6 @@
+import { formatCpf } from "../../../domain/helper/format-helper";
 import { TransactionRepository } from "../../../domain/interfaces/repositories/transaction-repository";
-import { CreateTransaction, ResponseTransaction } from "../../../domain/usecases/transaction-dto";
+import { CreateTransaction, ResponseTransaction, ResponseTransactionAvailable } from "../../../domain/usecases/transaction-dto";
 import { prisma } from "./prisma-client";
 
 export class TransactionPostgresRepository implements TransactionRepository {
@@ -7,6 +8,7 @@ export class TransactionPostgresRepository implements TransactionRepository {
     const transaction = await prisma.transaction.create({
       data: {
         value: data.value,
+        cpf: data.cpf,
         description: data.description,
         paymentMethod: data.paymentMethod,
         cardNumber: data.cardNumber,
@@ -27,5 +29,29 @@ export class TransactionPostgresRepository implements TransactionRepository {
 
   async findAll(): Promise<ResponseTransaction[]> {
     return await prisma.transaction.findMany();
+  }
+
+  async findTransactionsAvailable(cpf: string): Promise<ResponseTransactionAvailable[]> {
+    return await prisma.transaction.findMany({
+      select: {
+        payables: {
+          select: { value: true },
+        },
+      },
+      where: {
+        cpf,
+        payables: { status: 'paid' },
+      },
+    })
+  }
+
+  async findTransactionsWaitingFunds(cpf: string): Promise<ResponseTransaction[]> {
+    const formattedCpf = formatCpf(cpf)
+    return await prisma.transaction.findMany({
+      where: {
+        cpf: formattedCpf,
+        payables: { status: 'waiting_funds' },
+      }
+    })
   }
 }
