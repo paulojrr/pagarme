66e0355905e6ff7e3cda77b8e5531687e5b85735
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-03-29 20:13:29 -0300

    add get available e waiting funds

--- src/infra/api/rest/transaction.routes.ts
+++ src/infra/api/rest/transaction.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 import { CreateTransactionUseCase } from "../../../domain/usecases/create-transaction-usecase";
+import { GetAvailableTransactionUseCase } from "../../../domain/usecases/get-available-transaction-usecase";
 import { TransactionPostgresRepository } from "../../repositories/prisma/transaction-repository";
+import { validateGetTransaction } from "../validator/middlewares/validate-get-transaction-middleware";
 import { validateTransaction } from '../validator/middlewares/validate-transaction-middleware';
 import { TransactionController } from "./transaction-controller";
 
 const repository = new TransactionPostgresRepository()
-const useCase = new CreateTransactionUseCase(repository)
+const createTransactionUseCase = new CreateTransactionUseCase(repository)
+const getAvailableTransactionUseCase = new GetAvailableTransactionUseCase(repository)
 
-const transactionController = new TransactionController(useCase, repository)
+const transactionController = new TransactionController(createTransactionUseCase, getAvailableTransactionUseCase, repository)
 const transactionRoutes = Router();
 
-transactionRoutes.post("/transaction", validateTransaction, (request, response) =>  transactionController.create(request, response));
+transactionRoutes.post("/transaction", validateTransaction, (request, response) => transactionController.create(request, response));
+transactionRoutes.get("/transaction/available", validateGetTransaction, (request, response) => transactionController.getAvailable(request, response));
+transactionRoutes.get("/transaction/waitingFunds", validateGetTransaction, (request, response) => transactionController.getWaitingFunds(request, response));
 
 export { transactionRoutes };
