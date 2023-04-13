3bde5d2719caade31858d83a0087606355db22cd
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-04-13 02:54:46 -0300

    add test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'c8'
+    },
     exclude: [
       ...configDefaults.exclude,
       './out/**/*',
