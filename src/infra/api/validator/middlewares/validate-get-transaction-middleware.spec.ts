a254157bec70a4b889152745037e26c3011b06f6
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-04-13 03:16:00 -0300

    add tests validate get transaction

--- src/infra/api/validator/middlewares/validate-get-transaction-middleware.spec.ts
+++ src/infra/api/validator/middlewares/validate-get-transaction-middleware.spec.ts
@@ -3,13 +3,31 @@ import { describe, expect, it } from "vitest";
 import { app } from "../../server";
 
 describe("Validate get transaction middleware", async () => {
-  it("should have a valid payload", async () => {
+  it("should return a 200 status code if the cpf query parameter is present", async () => {
     const response = await request(app)
-      .get("/transaction")
-      .send({
-        cpf: "079.430.010-36",
-      });
+      .get("/transaction/available?cpf=07943001036");
     expect(response.status).toBe(200);
   });
 
+  it("should return a 200 status code if the cpf query parameter is present", async () => {
+    const response = await request(app)
+      .get("/transaction/waitingFunds?cpf=07943001036");
+    expect(response.status).toBe(200);
+  });
+
+  it("should return a 400 status code if the cpf query parameter is missing", async () => {
+    const response = await request(app)
+      .get("/transaction/available");
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toContain('"cpf" is required');
+  });
+
+  it("should return a 400 status code if the cpf query parameter is missing", async () => {
+    const response = await request(app)
+      .get("/transaction/waitingFunds");
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toContain('"cpf" is required');
+  });
 });
